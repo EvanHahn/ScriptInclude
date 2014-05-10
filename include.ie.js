03494eb903ee78ba8751d0006230b7fc8745c763
Christian César Robledo López <osiris2012@msn.com>
2014-05-10 09:04:53 -0500

    Update using byte saving techniques, added ie support version and the shortcut $i
    Update using byte saving techniques.
    Added comments.
    Added ie support version.
    Added the shortcut $i.
    The minified version goes from 311bytes to 257bytes.

--- include.ie.js
+++ include.ie.js
@@ -5,21 +5,24 @@ $i = window.include = function () {
 		c = a[n - 1],// last script or callback function
 		s,// script
 		i,
+		r = 'readyState',
 		d = document;
 
 	if (c.call)// if last element is a function
 	n--;
 
 	function l() { //function onloaded
+		if (!this[r] || /ded|te/.test(this[r])) { //if no ie or if ie readyState is equal to 'complete' or 'loaded' for ie compatibility
 			n--;
 			if (!n && c.call) // if all scripts are loaded and there is a callback function, call the function
 			c();
+		}
 	}
 
 	for (i = 0; i < n; i++) {
 		s = d.createElement("script");
 		s.src = a[i];
-		s.onload = s.onerror = l;
+		s.onload = s.onerror = s.onreadystatechange = l; //onreadystatechange for ie compatibility
 		d.getElementsByTagName('head')[0].appendChild(s);
 	}
 
